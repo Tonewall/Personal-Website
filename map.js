c85f781835be6599b13f07aeb7ced75c6f63d215
Tony Choi <tchoi52@gmail.com>
2019-04-04 17:20:51 -0400

    pc

--- map.js
+++ map.js
@@ -86,7 +86,7 @@ function click(clicked_id){
 
 };
 //for individual animations for each states on loadup
-//mac test 6
+//pc
 window.onload = function myMove() {
 
     var elem = document.getElementById("AK"); 
